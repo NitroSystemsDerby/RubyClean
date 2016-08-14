24d402c544cb955d258fbf480a7efcfd57dc3114
Ryan Pegg <ryan.pegg@nitrosystems.co.uk>
2016-08-14 17:58:14 +0100

    First Commit

---
+++ pages/index.php
@@ -0,0 +1,6 @@
+				<h3>A bit about us:</h3>
+				<hr />
+				<img src="IMG/home/main.jpg" alt="main image"/>
+				<p>Some random text about what we do and our back story, this can vary on what you want the target market is. If you was to go towards the upmarket house holds I would suggest using words such as high quality, reliable, trustworthy.</p>
+				<p>However, if you are looking to work for your average Joe then I would use phrases like resonably priced without lacking on the quality.</p> 
+			
